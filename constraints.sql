28bf19ea3abcf902907910e02dcb2a7d82e84947
Anas jaidi <anas.jaidi@icloud.com>
2023-03-11 11:09:30 +0100

    create table with constriants

--- constraints.sql
+++ constraints.sql
@@ -1,3 +1,8 @@
 CREATE TABLE human (
-  
+  id BIGSERIAL Not NULL,
+  first_name VARCHAR(50) Not NULL,
+  last_name VARCHAR(50) Not NULL,
+  gender VARCHAR(7) Not NULL,
+  date_of_birth date Not NULL,
+  email VARCHAR(150)
 );
